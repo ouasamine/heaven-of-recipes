37237a49d03aba8f3171fbc833d916dece3821de
ouasamine <ouas.amine@outlook.com>
2023-02-07 12:25:43 +0100

    Display food names created by signed in user

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,2 +1,5 @@
 class FoodController < ApplicationController
+  def index
+    @food = Food.where(user: current_user)
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,3 @@
 class Food < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
 end
