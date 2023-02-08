ecf00ffad866f332361ca9c69ff60c64d0989ee9
ouasamine <ouas.amine@outlook.com>
2023-02-08 10:37:31 +0100

    Create form to add new food on seperate page

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -2,4 +2,22 @@ class FoodController < ApplicationController
   def index
     @food = Food.where(user: current_user)
   end
+
+  def new
+    @new_food = Food.new
+  end
+
+  def create
+    @new_food = Food.new(food_params)
+
+    if @new_food.save
+      redirect_to food_index_path
+    end
+  end
+
+  private 
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
 end
