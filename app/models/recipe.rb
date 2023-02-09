23a3b14cd7623baa07fd787de947974dda8e05c8
ouasamine <ouas.amine@outlook.com>
2023-02-09 14:49:09 +0100

    Create missing_food method to return missing food details for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,20 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods
+  has_many :foods, through: :recipe_foods
+
+  def missing_food
+    data = {list: [], count: 0, total_price: 0}
+    recipe_foods.each do |recipe_food|
+      recipe_food_quantity = recipe_food.quantity
+      food_inv_quantity = recipe_food.food.quantity
+
+      if (food_inv_quantity - recipe_food_quantity) < 0
+        data[:list] << recipe_food.food
+        data[:count] += 1
+        data[:total_price] += recipe_food.food.price
+      end
+    end
+    data
+  end
 end
