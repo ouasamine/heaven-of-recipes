6b65c968da7bf15b9f74e236e5f4c5bac10bfd3e
ouasamine <ouas.amine@outlook.com>
2023-02-09 16:04:03 +0100

    Extract all recipes missing foods for signed user

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,13 +3,13 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
 
-  def missing_food
+  def missing_foods
     data = {list: [], count: 0, total_price: 0}
-    recipe_foods.each do |recipe_food|
+    recipe_foods.includes(:food).each do |recipe_food|
       recipe_food_quantity = recipe_food.quantity
       food_inv_quantity = recipe_food.food.quantity
 
-      if (food_inv_quantity - recipe_food_quantity) < 0
+      if (food_inv_quantity.to_i - recipe_food_quantity.to_i) < 0
         data[:list] << recipe_food.food
         data[:count] += 1
         data[:total_price] += recipe_food.food.price
