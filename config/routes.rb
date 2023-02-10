f1f22bf87910f80ce084e8bfd5e19312b540d8a0
AdnanOlarmmi <olarmmi@gmail.com>
2023-02-10 14:52:27 +0100

    Add Router for recipe

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   
   resources :food, only: [:index, :new, :create, :destroy]
   resources :general_shopping_list, only: [:index]
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
 
 end
